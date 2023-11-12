4b26c4412136de5646ce6938bab637d7fb011044
samezarus <samezarus@gmail.com>
2023-11-13 00:50:39 +1000

    init

--- app_employees/admin.py
+++ app_employees/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ["name", "parent", ]
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ["name", "parent", "state", ]
+
+
+@admin.register(Street)
+class StreetAdmin(admin.ModelAdmin):
+    list_display = ["name", "alt_name", "city", ]
+
+
+@admin.register(EmployeeGroup)
+class EmployeeGroupAdmin(admin.ModelAdmin):
+    list_display = ["name", "parent", ]
+
+
+admin.site.register(Employee)
--- app_employees/models.py
+++ app_employees/models.py
@@ -1,3 +1,99 @@
 from django.db import models
 
-# Create your models here.
+
+class State(models.Model):
+    parent = models.ForeignKey("self", verbose_name="родитель", on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(verbose_name="наименование", max_length=200, default="")
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Область"
+        verbose_name_plural = "Адреса | Области"
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    state = models.ForeignKey(
+        State,
+        verbose_name="область",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    parent = models.ForeignKey("self", verbose_name="родитель", on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(verbose_name="наименование", max_length=200, default="")
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Город"
+        verbose_name_plural = "Адреса | Города"
+
+    def __str__(self):
+        return self.name
+
+
+class Street(models.Model):
+    city = models.ForeignKey(
+        City,
+        verbose_name="город",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    name = models.CharField(verbose_name="наименование", max_length=200, default="")
+    alt_name = models.CharField(verbose_name="наименование2", max_length=200, default="")
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Улица"
+        verbose_name_plural = "Адреса | Улицы"
+
+    def __str__(self):
+        return self.name
+
+
+class EmployeeGroup(models.Model):
+    parent = models.ForeignKey("self", verbose_name="родитель", on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(verbose_name="наименование", max_length=200, default="")
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Должность"
+        verbose_name_plural = "Сотрудники | Должности"
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    employee_group = models.ForeignKey(
+        EmployeeGroup,
+        verbose_name="должность",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    employee_group = models.ForeignKey(
+        EmployeeGroup,
+        verbose_name="должность",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    
+    last_name = models.CharField(verbose_name="фамилия", max_length=100, default="")
+    first_name = models.CharField(verbose_name="имя", max_length=100, default="")
+    surname = models.CharField(verbose_name="отчество", max_length=100, default="")
+
+    class Meta:
+        ordering = ["last_name"]
+        verbose_name = "Сотрудник"
+        verbose_name_plural = "Сотрудники | Сотрудники"
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name} {self.surname}"
